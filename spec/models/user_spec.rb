0cb3f6901b07c884b24b4deed65746fd4b628d19
mycobble <mycobble@gmail.com>
2011-07-19 22:59:19 -0500

    No errors

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -27,7 +27,7 @@ describe User do
 	end
 
 	it "should accept valid email addresses" do
-		addresses = $w[user@foo.com THE_USER@foo.bar.org first.last@foo.jp]
+		addresses = %w[user@foo.com THE_USER@foo.bar.org first.last@foo.jp]
 		addresses.each do |address|
 			valid_email_user=User.new(@attr.merge(:email => address))
 			valid_email_user.should be_valid
@@ -35,7 +35,7 @@ describe User do
 	end
 
 	it "should reject invalid email addresses" do
-		addresses = $w[user@foo,com user_at_foo.org example.user@foo.]
+		addresses = %w[user@foo,com user_at_foo.org example.user@foo.]
 		addresses.each do |address|
 			valid_email_user=User.new(@attr.merge(:email => address))
 			valid_email_user.should_not be_valid
@@ -47,7 +47,6 @@ describe User do
 		User.create!(@attr)
 		user_with_duplicate_email=User.new(@attr)
 		user_with_duplicate_email.should_not be_valid
-		end
 	end
 
 	it "should reject email addresses identical up to case" do
@@ -55,7 +54,6 @@ describe User do
 		User.create!(@attr.merge(:email => upcased_email))
 		user_with_duplicate_email=User.new(@attr)
 		user_with_duplicate_email.should_not be_valid
-		end
 	end
 
 
